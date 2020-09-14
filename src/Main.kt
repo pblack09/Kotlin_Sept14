b6f089685c562499ec81e15d032dd24048284c61
Philip Black <philipblack12@gmail.com>
2020-09-14 11:28:41 -0500

    Added code comments

--- src/Main.kt
+++ src/Main.kt
@@ -6,10 +6,12 @@ fun main(args: Array<String>){
     var numberArray: IntArray = intArrayOf(1, 5, 7, 10, 2, 5)
     highestNum(numberArray)
 
+        // Savings Account
     var savings = SavingAccount(50)
     println(savings.deposit(350))
     println(savings.withdraw(200))
 
+        // Current Account (with error if withdrawing puts current amount less than 100)
     var current = CurrentAccount(120)
     println(current.deposit(10))
     println(current.withdraw(10))
